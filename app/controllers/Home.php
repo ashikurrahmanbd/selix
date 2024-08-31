611852fe470afee316350c8016aa12e4196dc4ef
Ashikur Rahman <proashik012@gmail.com>
2024-08-31 13:31:11 +0600

    New filee added

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,7 +4,14 @@ class Home extends Controller{
 
     
     public function index(){
-        
+
+        $model = new Model;
+        $arr['id']  = 1;
+
+        $result = $model->where($arr);
+
+        seewhat($result);
+     
         $this->view('home');
 
     }
--- app/core/config.php
+++ app/core/config.php
@@ -1,10 +1,22 @@
 <?php
 
 if($_SERVER['SERVER_NAME'] == 'localhost'){
+
+    define('DBNAME', 'selix');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBDRIVER', '');
     
     define('ROOT', 'http://localhost/selix/public');
 
 }else{
+
+    define('DBNAME', 'selix');
+    define('DBUSER', 'root');
+    define('DBHOST', '');
+    define('DBDRIVER', '');
+
     define('ROOT', 'https://yourdomaian.com');
 }
 
